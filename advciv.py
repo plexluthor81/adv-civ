ac17668a3924e363e6640df7a55c80a9256817d9
Steve Markham <steve.markham@gmail.com>
2021-01-10 19:14:58 -0500

    playing around with buttons, stock, and treasury

--- advciv.py
+++ advciv.py
@@ -1,13 +1,16 @@
 from kivy.app import App
 from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.gridlayout import GridLayout
 from kivy.uix.scatter import Scatter
 from kivy.uix.label import Label
-from kivy.properties import NumericProperty, ListProperty
+from kivy.uix.button import Button
+from kivy.properties import NumericProperty, ListProperty, StringProperty
 from kivy.graphics.vertex_instructions import (Rectangle,
                                                Ellipse,
                                                Line)
 from kivy.graphics.context_instructions import Color
 from kivy.clock import Clock
+from kivy.uix.screenmanager import Screen, ScreenManager
 
 from math import sqrt
 
@@ -44,7 +47,6 @@ def get_snap_pos(window_pos):
 
 class Spotter(Label):
     def on_touch_up(self, touch):
-        res = super(Spotter, self).on_touch_up(touch)
         if self.collide_point(*touch.pos):
             map_pos = window_to_map(self.parent.pos)
             print(f"{map_pos[0]:.1f},{map_pos[1]:.1f}")
@@ -52,12 +54,14 @@ class Spotter(Label):
                 territory, snap_pos = get_snap_pos(self.parent.pos)
                 self.parent.pos = snap_pos
                 print(f"Snapping to {territory}")
-        return res 
+                return True
+        return super(Spotter, self).on_touch_up(touch) 
 
 class AstTokenWidget(Label):
     ast = NumericProperty(0)
     track = NumericProperty(0)
     color = ListProperty([0, 0, 0, 1])
+    target_size = 48
 
     def __init__(self, **kwargs):
         super(AstTokenWidget, self).__init__(**kwargs)
@@ -67,15 +71,15 @@ class AstTokenWidget(Label):
         self.parent.parent.bind(size=self.update_size)        
 
     def get_window_pos(self):
-        map_pos = (202 + self.ast*60, 22 + self.track*60)
+        map_pos = (208 + self.ast*60, 28 + self.track*60)
         return map_to_window(map_pos)
 
     def get_window_size(self):
         app = App.get_running_app()
         if not app.root:
-            return (58,58)
+            return (self.target_size,self.target_size)
         map_widget = app.root.ids['map']
-        return (58/4058.0*map_widget.size[0], 58/2910.0*map_widget.size[1])
+        return (self.target_size/4058.0*map_widget.size[0], self.target_size/2910.0*map_widget.size[1])
     
     def on_touch_down(self, touch):
         if self.collide_point(*touch.pos):
@@ -92,10 +96,58 @@ class AstTokenWidget(Label):
     def update_size(self, instance, size):
         app = App.get_running_app()
         map_widget = app.root.ids['map']
-        self.size = (60/4058.0*map_widget.size[0], 60/2910.0*map_widget.size[1])
+        self.size = (self.target_size/4058.0*map_widget.size[0], self.target_size/2910.0*map_widget.size[1])
         self.pos = self.get_window_pos()
     
+class NationButton(Button):
+    def on_press(self):
+        app = App.get_running_app()
+        old_an = app.active_nation
+        app.active_nation = self.text
+        print(f"{old_an}->{app.active_nation}")
 
+class TurnOrderScreen(Screen):
+    def __init__(self, **kwargs):
+        super(TurnOrderScreen, self).__init__(**kwargs)
+        self.name = 'Turn Order'
+        Clock.schedule_once(self.init_ui, 0)
+
+    def init_ui(self, *args):
+        gl = GridLayout(cols=3)
+        print(gl.size)
+        self.stock_label = Label(width=self.parent.size[0]/3,height=self.parent.size[1],valign='top',halign='center',text="Stock")
+        self.stock_label.text_size = self.stock_label.size
+        phase_label = Label(font_size=8, markup=True,text="""[b][size=10]Phase Descriptions[/size][/b]
+1. Collect Taxes
+2. Population Expanstion
+3. Census
+4. Build Ships
+5. Movement
+6. Conflict
+7. Build Cities
+8. Remove Surplus Population
+9. Acquire Trade Cards
+10. Trade
+11. Resolve Calamities
+12. Acquire Civ Cards
+13. Alter AST""")
+        treas_label = Label(text="Treasury")
+        gl.add_widget(self.stock_label)
+        gl.add_widget(phase_label)
+        gl.add_widget(treas_label)
+        print(gl.size)
+        print(self.size)
+        with gl.canvas.before:
+            Color(.7, .7, .7, 1)
+            Rectangle(pos=(0,0), size=self.parent.size)
+        with self.stock_label.canvas.before:
+            Color(.3, .3, .3, 1)
+            Rectangle(pos=self.stock_label.pos, size=self.stock_label.size)
+        self.add_widget(gl)
+
+    def on_touch_down(self, touch):
+        if self.stock_label.collide_point(*touch.pos):
+            print("You poked stock")
 
 class MapScatter(Scatter):
     def on_transform_with_touch(self, touch):
@@ -131,6 +183,11 @@ class AdvCivWidget(BoxLayout):
 
 
 class AdvCivApp(App):
+    active_nation = StringProperty("Not Set")
+
+    def rgba_tuple(self, rgb, a):
+        return tuple([x/255 for x in rgb] + [a])
+
     def build(self):
         return AdvCivWidget()
 
